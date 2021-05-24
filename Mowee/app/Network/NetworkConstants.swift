011fd23fd8135315a9f5cdb2097187c9ad8b6fa2
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-05-24 15:23:08 +0100

    Add api key

---
+++ Mowee/app/Network/NetworkConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkConstants.swift
+//  Mowee
+//
+//  Created by Mojisola Adebiyi on 24/05/2021.
+//
+
+import Foundation
+
+enum NetworkConstants: String {
+    case apiKey = "144a73d4f608ecf606e7214c24cbd49b"
+}
