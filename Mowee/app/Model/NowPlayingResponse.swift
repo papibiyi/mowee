dc3209a8ae853d65a5ac0aa3ca57d04fd2b258c7
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-05-25 14:14:06 +0100

    Add models

---
+++ Mowee/app/Model/NowPlayingResponse.swift
@@ -0,0 +1,12 @@
+//
+//  NowPlayingResponse.swift
+//  Mowee
+//
+//  Created by Mojisola Adebiyi on 24/05/2021.
+//
+
+import Foundation
+
+struct NowPlayingResponse: Codable {
+    let results: [Movie]?
+}
