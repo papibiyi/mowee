609f6393bf5a3698fc0aa376bd18a8cd8a8e449a
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-05-25 14:15:02 +0100

    Add image cache manager

---
+++ Mowee/app/Custom/class/ImageCacheManager.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCacheManager.swift
+//  Mowee
+//
+//  Created by Mojisola Adebiyi on 25/05/2021.
+//
+
+import Foundation
+
+class ImageCacheManager {
+    static let cache = NSCache<NSString, NSData>()
+
+    static func fetchImageData(from url: String, completion: @escaping (NSData) -> (Void)){
+        let key = NSString(string: url)
+        if let cachedVersion = cache.object(forKey: key) {
+            DispatchQueue.global(qos: .background).async {
+                completion(cachedVersion)
+            }
+        } else {
+            DispatchQueue.global(qos: .background).async {
+                guard let url = URL(string: url) else {return}
+                guard let ddata = try? Data(contentsOf: url) else {return}
+                let ns = NSData(data: ddata)
+                cache.setObject(ns, forKey: key)
+                completion(ns)
+            }
+        }
+    }
+    
+}
