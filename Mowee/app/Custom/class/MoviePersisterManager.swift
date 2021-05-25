5c5c9e6dab9651760b21eaeefdfc9e8845598132
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-05-25 21:32:43 +0100

    Add tests

--- Mowee/app/Custom/class/MoviePersisterManager.swift
+++ Mowee/app/Custom/class/MoviePersisterManager.swift
@@ -18,18 +18,22 @@ class MoviePersisterManager {
     }
     
     private static var fileURL: URL? {
-        return documentsFolder?.appendingPathComponent("favouriteMovie.data")
+        if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] == nil {
+            return documentsFolder?.appendingPathComponent("favouriteMovie.data")
+        }else {
+            return documentsFolder?.appendingPathComponent("test.data")
+        }
     }
     
-    func load() -> [Movie]? {
-        guard let url = MoviePersisterManager.fileURL else { return nil }
-        guard let data = try? Data(contentsOf: url) else { return nil }
-        guard let favourite = try? JSONDecoder().decode(Favourites.self, from: data) else { return nil }
-        return favourite.movies
+    func load() -> [Movie] {
+        guard let url = MoviePersisterManager.fileURL else { return [] }
+        guard let data = try? Data(contentsOf: url) else { return [] }
+        guard let favourite = try? JSONDecoder().decode(Favourites.self, from: data) else { return [] }
+        return favourite.movies ?? []
     }
     
     func save(movie: Movie) {
-            var movies = self.load() ?? []
+            var movies = self.load()
             guard !movies.contains(where: { (data) -> Bool in
                 data.id == movie.id
             }) else {return}
@@ -40,10 +44,16 @@ class MoviePersisterManager {
     }
     
     func remove(movie: Movie) {
-        var movies = self.load() ?? []
+        var movies = self.load()
         guard let index = movies.firstIndex(where: { $0.id == movie.id}) else {return}
         movies.remove(at: index)
         guard let data = try? JSONEncoder().encode(Favourites(movies: movies)) else { fatalError("Error encoding data") }
+        guard let url = MoviePersisterManager.fileURL else { return }
+        try? data.write(to: url)
+    }
+    
+    func clear() {
+        guard let data = try? JSONEncoder().encode(Favourites(movies: [])) else { fatalError("Error encoding data") }
         guard let url = MoviePersisterManager.fileURL else { return }
         try? data.write(to: url)
     }
--- MoweeTests/MoweeTests.swift
+++ MoweeTests/MoweeTests.swift
@@ -8,25 +8,106 @@
 import XCTest
 
 class MoweeTests: XCTestCase {
+    
+    var movies: [Movie]!
+    
+    let item1 = Movie(id: 0, adult: nil, backdropPath: nil, genreIDS: nil, originalLanguage: nil, originalTitle: nil, overview: nil, popularity: nil, posterPath: nil, releaseDate: nil, title: "one", video: nil, voteAverage: nil, voteCount: nil, genres: nil)
+    
+    let item2 = Movie(id: 1, adult: nil, backdropPath: nil, genreIDS: nil, originalLanguage: nil, originalTitle: nil, overview: nil, popularity: nil, posterPath: nil, releaseDate: nil, title: "two", video: nil, voteAverage: nil, voteCount: nil, genres: nil)
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        movies = MoviePersisterManager.shared.load()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        movies = []
+        MoviePersisterManager.shared.clear()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_movies_empty() throws {
+        XCTAssertEqual(movies, [])
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_count1() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 1)
     }
-
+    
+    func test_count2() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 2)
+    }
+    
+    func test_count_same_item() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item1)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 1)
+    }
+    
+    func test_remove_count1() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item1)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 1)
+    }
+    
+    func test_remove_count2() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item1)
+        MoviePersisterManager.shared.remove(movie: item2)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 0)
+    }
+    
+    func test_find_item1() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        XCTAssertTrue(MoviePersisterManager.shared.load().contains(where: {$0 == item1}))
+    }
+    
+    func test_find_item2() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        XCTAssertTrue(MoviePersisterManager.shared.load().contains(where: {$0 == item2}))
+    }
+    
+    func test_confirm_remove_item1() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item1)
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item1}))
+    }
+    
+    func test_confirm_remove_item2() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item2)
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item2}))
+    }
+    
+    func test_confirm_remove_both() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item2)
+        MoviePersisterManager.shared.remove(movie: item1)
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item2}))
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item1}))
+    }
+    
+    func test_insertDuplicate() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.save(movie: item2)
+        XCTAssertEqual(MoviePersisterManager.shared.load().count, 2)
+    }
+    
+    func test_clear() throws {
+        MoviePersisterManager.shared.save(movie: item1)
+        MoviePersisterManager.shared.save(movie: item2)
+        MoviePersisterManager.shared.clear()
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item2}))
+        XCTAssertFalse(MoviePersisterManager.shared.load().contains(where: {$0 == item1}))
+        XCTAssertEqual(MoviePersisterManager.shared.load(), [])
+    }
 }
